79aebc59c456d945cad7a364b73bb0cde33fbd17
Juno <junonguyen27@gmail.com>
2022-02-02 11:23:38 -0600

    added baseline routes and role model

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,11 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const projectRoutes = require('./projectRoutes');
 
+router.use('/', baseRoute);
+router.use('/schedule', scheduleRoute);
 
+module.exports = router;
 
 //****EXAMPLE "api/index.js" FROM UNIT 14 ACTIVITY 17 ***/
 // const router = require('express').Router();
---
+++ controllers/api/scheduleRoute.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+//homepage, {user} is a placeholder, will pass respective logged in employee's schedule 
+router.get('/', async (req, res) => {
+    res.render('timeoff', {user});
+});
+//route for employee to view check
+router.get('/check', async (req, res) => {
+    return res.render('check', {user});
+});
+
+module.exports = router;
