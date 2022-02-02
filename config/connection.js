cbb6a44682d8f365a32fc1fc6f4b85cc737dc1f0
Leo Dickens <dickens.leo@gmail.com>
2022-02-02 13:54:07 -0600

    fixing start script

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,21 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    //***CHANGED PORT FROM 3306 TO 3333 ***/
-    port: 3306,
-  }
-);
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
 
 module.exports = sequelize;
-
-//***CODE PULLED FROM ACTIVITY 17 UNIT 14 ***/
