23b2bbfaa25bacac1ea0a6a75d631c4639992207
Juno <junonguyen27@gmail.com>
2022-02-02 13:27:32 -0600

    accidentally worked in main branch

--- controllers/api/baseRoute.js
+++ controllers/api/baseRoute.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 //homepage, {user} is a placeholder 
 router.get('/', async (req, res) => {
-    res.render('login');
+    res.render('login',);
 });
 //dashboard route, user is placeholder
-router.get('/dashboard', async (req, res) => {
-    return res.render('main');
-});
+// router.get('/dashboard', async (req, res) => {
+//     return res.render('main');
+// });
 
 module.exports = router;
--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,14 +1,14 @@
-const router = require('express').Router();
-// const { User, Timesheet } = require('../models'); THIS IS A PLACEHOLDER FOR OUR MODELS 
+// const router = require('express').Router();
+// // const { User, Timesheet } = require('../models'); THIS IS A PLACEHOLDER FOR OUR MODELS 
 
-router.get('/login', (req, res) => {
-    // If the user is already logged in, redirect to the homepage
-    if (req.session.loggedIn) {
-      res.redirect('/');
-      return;
-    }
-    // Otherwise, render the 'login' template
-    res.render('welcome');
-});
+// router.get('/login', (req, res) => {
+//     // If the user is already logged in, redirect to the homepage
+//     if (req.session.loggedIn) {
+//       res.redirect('/');
+//       return;
+//     }
+//     // Otherwise, render the 'login' template
+//     res.render('welcome');
+// });
 
-module.exports = router;
+// module.exports = router;
