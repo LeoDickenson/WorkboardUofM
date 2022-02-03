5929a49fd5f6b94a92c824124576b321e13c9221
Juno <junonguyen27@gmail.com>
2022-02-03 13:31:01 -0600

    handlebars fixed

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,16 +2,7 @@ const router = require('express').Router();
 const signupRoute = require('./signup');
 // const projectRoutes = require('./projectRoutes');
 
-router.get('/', signupRoute);
+router.use('/signup', signupRoute);
 // router.use('/schedule', scheduleRoute);
 
 module.exports = router;
-
-//****EXAMPLE "api/index.js" FROM UNIT 14 ACTIVITY 17 ***/
-// const router = require('express').Router();
-
-// const userRoutes = require('./user-routes');
-
-// router.use('/users', userRoutes);
-
-// module.exports = router;
