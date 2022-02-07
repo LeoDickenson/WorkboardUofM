c2ae0cdb72db6b45ae5e80778c94c5b138abe720
Leo Dickens <dickens.leo@gmail.com>
2022-02-07 14:57:28 -0600

    fix seed

--- seeds/adminData.js
+++ seeds/adminData.js
@@ -1,7 +1,7 @@
 // here enter a few dummy useres including a manager
 // maybe create an admin user that has manager credentials (something the company IT guy would use)
 
-const Employee = require('../models/Employee');
+const Employee = require('../models/employee');
 
 const userdata = [
     {
