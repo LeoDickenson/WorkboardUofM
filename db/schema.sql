430e773d2f6747d2a7979884f7815030c3312e21
Juno <junonguyen27@gmail.com>
2022-02-07 19:55:06 -0600

    testing

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
 -- workforce meaning the employees and managers
-DROP DATABASE IF EXISTS workforce_db;
+-- DROP DATABASE IF EXISTS workforce_db;
 
-CREATE DATABASE workforce_db;
+-- CREATE DATABASE workforce_db;
 
-USE workforce_db;
+USE pww0x2sl0dgxsivj
